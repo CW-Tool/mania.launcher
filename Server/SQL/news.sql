c6fc915be25b0f9abaf0ac7863dd49a7313613f8
fellipesac <fellipesac@outlook.com>
2017-04-21 15:38:31 -0300

    Atualizações no arquivo de SQL

--- Server/SQL/news.sql
+++ Server/SQL/news.sql
@@ -1,5 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mania
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
 
-CREATE TABLE IF NOT EXISTS `news_launcher` (
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hot_news`
+--
+
+DROP TABLE IF EXISTS `hot_news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hot_news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text,
+  `realms` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hot_news`
+--
+
+LOCK TABLES `hot_news` WRITE;
+/*!40000 ALTER TABLE `hot_news` DISABLE KEYS */;
+INSERT INTO `hot_news` VALUES (1,'O servidor está com 2x EXP e MESOS! Corram e aproveitem!','realm1');
+/*!40000 ALTER TABLE `hot_news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `news_launcher`
+--
+
+DROP TABLE IF EXISTS `news_launcher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news_launcher` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `message` varchar(10000) NOT NULL,
@@ -8,24 +56,27 @@ CREATE TABLE IF NOT EXISTS `news_launcher` (
   `realms` varchar(255) NOT NULL,
   `date_time` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `news_launcher` (`id`, `title`, `message`, `newslink`, `imagelink`, `realms`, `date_time`) VALUES
-(1, 'Новый комикс: «Сумерки в Сурамаре»', 'Автор, Мэтт Бернс (Matt Burns) — старший сценарист Blizzard Entertainment и соавтор, в частности, «Хроник Варкрафт». Художник Людо Луллаби (Ludo Lullabi) выпустил более двадцати альбомов и неоднократно работал с Blizzard, в том числе над комиксами по мотивам World of Warcraft.', 'http://127.0.0.1/', 'http://127.0.0.1/launcher/1.jpg', 'realm1', '13 October 2016');
+--
+-- Dumping data for table `news_launcher`
+--
 
+LOCK TABLES `news_launcher` WRITE;
+/*!40000 ALTER TABLE `news_launcher` DISABLE KEYS */;
+INSERT INTO `news_launcher` VALUES (1,'Launcher do Servidor','A inauguração do servidor está chegando, e pra isso, todos os preparativos já estão sendo feitos! Hoje, você pode ver nosso fantástico launcher :D','http://127.0.0.1/','http://127.0.0.1/launcher/news_image/1.jpg','realm1','20 de Outubro de 2017');
+/*!40000 ALTER TABLE `news_launcher` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `hot_news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` text,
-  `realms` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `users`
+--
 
-INSERT INTO `hot_news` (`id`, `message`, `realms`) VALUES
-(1, 'Новый комикс: «Сумерки в Сурамаре»', 'realm1');
-
-
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `realname` varchar(128) DEFAULT NULL,
   `username` varchar(45) NOT NULL,
@@ -35,7 +86,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `users` (`id`, `realname`, `username`, `password`, `email`, `role`) VALUES
-(1, 'Nostale', 'admin', '3f5cbfff0a2c6d067e53a582f08799652e24b446c50fedaee28e335d7f1a8527', 'admin@admin.ru', 'admin'),
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'MapleStory Mania','admin','3f5cbfff0a2c6d067e53a582f08799652e24b446c50fedaee28e335d7f1a8527','admin@maplestorymania.com.br','admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-21 15:37:35
